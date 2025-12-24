6311e9e5d6d746fe50ca8100f38b3df2cc49def6
Rayan <rayan.saoud@uha.fr>
2025-12-24 13:48:39 +0100

    Finalisation : Ajout des requirements et commentaires de config

--- src/common/config.py
+++ src/common/config.py
@@ -1,3 +1,6 @@
-# Config partagée
-MASTER_IP = '192.168.1.143' #  IP de la VM Linux ici
+# Configuration du Réseau
+# Si vous testez en local (1 seule machine), mettez '127.0.0.1'
+# Si vous testez sur plusieurs VMs, mettez l'IP de la machine Master ci-dessous.
+
+MASTER_IP = '192.168.1.143' #  <--- A modifier selon votre IP Master
 MASTER_PORT = 5000
